3f8b94a94e453da42cdbc249c230606179bb186c
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2024-05-09 20:47:16 +0200

    add picutres, and adress

--- public/scripts/pictures.js
+++ public/scripts/pictures.js
@@ -1,9 +1,15 @@
 const pictures = [
   "/andree/public/assets/images/4719BB41-4909-4865-96C2-01E776AFFB3F.jpeg",
+  "/andree/public/assets/images/29C05F0A-41F3-42A6-988E-784829E4E909.jpeg",
+  "/andree/public/assets/images/4728084A-F48F-4DCD-8E65-480DFA995E49.jpeg",
+  "/andree/public/assets/images/C6F2DA0D-50CC-4CAB-8B8A-B68E31EA7EAD.jpeg",
+  "/andree/public/assets/images/B8CB87CF-D3D2-41A7-8BE5-6A15790421FA.jpeg",
   "/andree/public/assets/images/F14F485C-5691-4F88-94AC-45993DCC2E80.jpeg",
   "/andree/public/assets/images/F6FAEFA4-D88A-449E-A7F4-C5D8A5FC70D4.jpeg",
   "/andree/public/assets/images/BC16B188-50F2-4D9F-BBCE-82F407AD6ED2.jpeg",
   "/andree/public/assets/images/C5305E65-D654-488D-9BD3-BE2525C71D1F.jpeg",
+  "/andree/public/assets/images/C0E553DD-4017-4233-A4D7-F1ECF0BC933A.jpeg",
+  "/andree/public/assets/images/56311F79-ECE2-4057-8A0E-3CE27538548C.jpeg",
 ];
 
 export default pictures;
